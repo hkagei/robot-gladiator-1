249117acb97668fa7d4d537079ddcf19eeb482b9
hkagei <k_dawg_1022@hotmail.com>
2022-03-29 21:56:41 -0600

    high score

--- assets/js/game.js
+++ assets/js/game.js
@@ -160,20 +160,35 @@ var fight = function(enemy) {
     isPlayerTurn = !isPlayerTurn;
   }
 };
-var endGame = function () {
-  // if player is still alive, player wins!
-  if (playerInfo.health > 0) {
-    window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".")
-  }
-  else {
-    window.alert("You've lost your robot in battle.");
-  }
-  var randomNumber = function() {
-    var value = Math.floor(Math.random() * 21) + 40;
+var endGame = function() {
+  window.alert("The game has now ended. Let's see how you did!");
 
-    return value
-  };
-}
+  // check localStorage for high score, if it's not there, use 0
+  var highScore = localStorage.getItem("highscore");
+  if (highScore === null) {
+    highScore = 0;
+  }
+  // if player has more money than the high score, player has new high score!
+  if (playerInfo.money > highScore) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
+
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+  } 
+  else {
+    alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+  }
+
+  // ask player if they'd like to play again
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+
+  if (playAgainConfirm) {
+    startGame();
+  } 
+  else {
+    window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+  }
+};
 var startGame = function() {
   // reset player stats
   playerInfo.health = 100;
